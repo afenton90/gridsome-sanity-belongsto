f5ada88bf3b877e89e345055881fd029f99d8004
afenton90 <afenton90@gmail.com>
2020-06-14 10:21:54 +0100

    feat(studio): init sanity studio

---
+++ studio/schemas/genre.js
@@ -0,0 +1,28 @@
+import { FaTheaterMasks } from "react-icons/fa";
+
+export default {
+  name: "genre",
+  title: "Genre",
+  type: "document",
+  icon: FaTheaterMasks,
+  fields: [
+    {
+      name: "title",
+      title: "Title",
+      type: "string",
+    },
+    {
+      name: "slug",
+      title: "Slug",
+      type: "slug",
+      options: {
+        source: "title",
+      },
+    },
+    {
+      name: "overview",
+      title: "Overview",
+      type: "blockContent",
+    },
+  ],
+};
